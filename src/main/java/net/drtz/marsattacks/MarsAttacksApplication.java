fdef6b6c3aa6164d9af8b307f78768b1782727a5
danny <danny@mba.home.drtz.net>
2020-01-30 22:38:49 -0600

    Scaffold with Spring Initializer

---
+++ src/main/java/net/drtz/marsattacks/MarsAttacksApplication.java
@@ -0,0 +1,13 @@
+package net.drtz.marsattacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsAttacksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsAttacksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/drtz/marsattacks/MarsAttacksApplicationTests.java
@@ -0,0 +1,13 @@
+package net.drtz.marsattacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsAttacksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
